bd86a1e9ccc7c4e745f65967eb1888cda2eb2e8f
hlatorreg <hector.latorre23@gmail.com>
2022-06-09 00:05:30 -0400

    Entry and output models, basic method for deciding who is player 1

--- src/talana_kombat/api/kombat/business.py
+++ src/talana_kombat/api/kombat/business.py
@@ -1,1 +1,51 @@
 """Business logic, or fight choreography"""
+MAX_HEALTH = 6
+MAX_MOVEMENTS = 5
+MAX_STRIKES = 1
+ALLOWED_MOVEMENTS = "WSAD"
+ALLOWED_STRIKES = "PK"
+
+TONYN_COMBINATIONS = {
+    "Taladoken": {"MOVEMENTS": "DSD", "STRIKE": "P", "DAMAGE": 3},
+    "Remuyuken": {"MOVEMENTS": "SD", "STRIKE": "K", "DAMAGE": 2},
+    "Strike": {"MOVEMENTS": "", "STRIKE": "PK", "DAMAGE": 1},
+}
+
+ARNALDOR_COMBINATIONS = {
+    "Remuyuken": {"MOVEMENTS": "SA", "STRIKE": "K", "DAMAGE": 3},
+    "Taladoken": {"MOVEMENTS": "ASA", "STRIKE": "P", "DAMAGE": 2},
+    "Strike": {"MOVEMENTS": "", "STRIKE": "PK", "DAMAGE": 1},
+}
+
+
+def get_relato(kombat: dict) -> list:
+    narration = []
+    first_player = define_first(kombat)
+    narration.append(
+        f"First player is {first_player}, movements are {kombat[first_player]}"
+    )
+    return narration
+
+
+def define_first(kombat: dict) -> str:
+    mov_tony = len(kombat["player1"]["movimientos"])
+    gol_tony = len(kombat["player1"]["golpes"])
+    mov_arnal = len(kombat["player2"]["movimientos"])
+    gol_arnal = len(kombat["player2"]["golpes"])
+    comb_tony = mov_tony + gol_tony
+    comb_arnal = mov_arnal + gol_arnal
+
+    if comb_tony != comb_arnal:
+        return "player1" if comb_tony > comb_arnal else "player2"
+
+    if mov_tony != mov_arnal:
+        return "player1" if mov_tony > mov_arnal else "player2"
+
+    if gol_tony != gol_arnal:
+        return "player1" if gol_tony > gol_arnal else "player2"
+
+    return "player1"
+
+
+def generate_fight_report():
+    return
--- src/talana_kombat/api/kombat/endpoints.py
+++ src/talana_kombat/api/kombat/endpoints.py
@@ -4,17 +4,36 @@ from http.client import OK
 
 from flask_restx import Namespace, Resource
 
+from talana_kombat.api.kombat.business import get_relato
+
+from talana_kombat.api.kombat.dto import (
+    fields_model,
+    player_model,
+    response_model,
+    kombat_reqparser,
+    player1_reqparser,
+    player2_reqparser
+)
+
 kombat_ns = Namespace(name="kombat", validate=True)
+kombat_ns.models[fields_model.name] = fields_model
+kombat_ns.models[player_model.name] = player_model
+kombat_ns.models[response_model.name] = response_model
 
 
 @kombat_ns.route("/start", endpoint="kombat_start")
 class StartKombat(Resource):
     """Handles HTTP requests to URL: /api/v1/kombat/start."""
 
-    @kombat_ns.response(int(HTTPStatus.ACCEPTED), "Kombat as begun!")
+    @kombat_ns.expect(kombat_reqparser)
+    # @kombat_ns.marshal_with(player_model)
+    @kombat_ns.response(int(HTTPStatus.OK), "Kombat as begun!", response_model)
     @kombat_ns.response(int(HTTPStatus.BAD_REQUEST), "Validation error.")
     @kombat_ns.response(int(HTTPStatus.INTERNAL_SERVER_ERROR),
                         "Internal server error.")
     def post(self):
         """Fight!"""
-        return OK
+        fight_args = kombat_reqparser.parse_args()
+        player1_reqparser.parse_args(req=fight_args)
+        player2_reqparser.parse_args(req=fight_args)
+        return get_relato(fight_args), OK
